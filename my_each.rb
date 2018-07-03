685b735d421a0d0f3a85cf698f9b2cd6e38b23d1
Josue V. Alorro <joalorro@gmail.com>
2018-07-03 16:07:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+array = ["if", "she", "brings", "me", "to", "my", "knees", "it's", "a", "bad", "religion"]
+
+def my_each array 
+  i = 0 
+  while i < array.length 
+    yield array[i]
+    i += 1
+  end
+  array
 end
+
+my_each array do |lyrics|
+  lyrics
+end
+
